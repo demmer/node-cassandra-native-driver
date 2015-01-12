941bb6bc4e7105b830fda7751dbb7635a3efaeb8
Michael Demmer <demmer@jut.io>
2015-01-12 14:02:25 -0800

    fix mem test

--- test/mem.js
+++ test/mem.js
@@ -1,34 +1,41 @@
-var cassandra = require('../build/Debug/cassandra-native');
+var cassandra = require('../build/Release/cassandra-native');
 var lib = require('..');
 
 setTimeout(function() { console.log('Done');}, 1000);
 
 var c = new cassandra.Client();
-c.connect(function() { console.log('connected');  } );
+c.connect('127.0.0.1', function() {
+    console.log('connected');
 
-var q = c.new_query();
+    var q = c.new_query();
 
-try {
-    q.parse('select * from kairosdb.string_index limit 1', []);
-    q.execute({}, function(err, results) {
-        console.log('execute done', err, results);
-        global.gc();
-    });
-} catch(err) {
-    console.error('got err', err.stack);
-}
+    try {
+        q.parse('select * from kairosdb.string_index limit 1', []);
+        q.execute({}, function(err, results) {
+            console.log('execute done', err, results);
+            global.gc();
+            c = null;
+        });
+    } catch(err) {
+        console.error('got err', err.stack);
+    }
 
+});
 
 var c2 = new lib.Client();
-c.connect(function() { console.log('connected');  } );
+c2.connect('127.0.0.1', function() {
+    console.log('connected');
 
-try {
-    c2.execute('select * from kairosdb.string_index limit 1', function(err, results) {
-        console.log('c2 execute done', err, results);
-        global.gc();
-    });
-} catch(err) {
-    console.error('got err', err);
-}
+    try {
+        c2.execute('select * from kairosdb.string_index limit 1', function(err, results) {
+            console.log('c2 execute done', err, results);
+            c2.client = null;
+            c2 = null;
+            global.gc();
+        });
+    } catch(err) {
+        console.error('got err', err);
+    }
+});
 
 setTimeout(function() { console.log('gc timeout'); global.gc(); });
