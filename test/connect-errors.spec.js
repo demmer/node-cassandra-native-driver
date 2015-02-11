a8a280e1a87363f9137e2a3d398a4639745411c4
Michael Demmer <demmer@jut.io>
2015-02-11 15:05:41 -0800

    free the session if connect fails

---
+++ test/connect-errors.spec.js
@@ -0,0 +1,20 @@
+var TestClient = require('./test-client');
+var _ = require('underscore');
+var Promise = require('bluebird');
+var expect = require('chai').expect;
+
+describe('connection error handling', function() {
+    it('fails to connect repeatedly to a non-listening address', function() {
+        var client = new TestClient();
+        this.timeout(30000);
+        return Promise.each(_.range(10001, 10105), function(port) {
+            return client.connect({address: '127.0.0.1', port: port})
+            .then(function() {
+                throw new Error('unexpected success');
+            })
+            .catch(function(err) {
+                expect(err.message).equal('No hosts available');
+            });
+        });
+    });
+});
