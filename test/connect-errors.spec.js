71bca618fe484847013caefdc1a3c0f14010a8a3
Michael Demmer <demmer@jut.io>
2015-06-01 06:54:49 -0700

    connect-errors: fix test to connect to each port sequentially with delay

--- test/connect-errors.spec.js
+++ test/connect-errors.spec.js
@@ -7,14 +7,15 @@ describe('connection error handling', function() {
     it('fails to connect repeatedly to a non-listening address', function() {
         var client = new TestClient();
         this.timeout(30000);
-        return Promise.each(_.range(10001, 10105), function(port) {
+        return Promise.each(_.range(10001, 10025), function(port) {
             return client.connect({address: '127.0.0.1', port: port})
             .then(function() {
                 throw new Error('unexpected success');
             })
             .catch(function(err) {
                 expect(err.message).equal('No hosts available for the control connection');
-            });
+            })
+            .delay(10);
         });
     });
 });
