cc489d7ea2d18ab2cb19a8e5b2cf381a389f3c4b
Michael Demmer <demmer@jut.io>
2015-10-12 05:29:16 -0700

    test: update tests to the new API

--- test/type-encodings.spec.js
+++ test/type-encodings.spec.js
@@ -1,6 +1,7 @@
 var Promise = require('bluebird');
 var TestClient = require('./test-client');
 var types = TestClient.types;
+var encodings = TestClient.encodings;
 var expect = require('chai').expect;
 var table = 'test';
 var _ = require('underscore');
@@ -9,7 +10,7 @@ var setup_environment = require('./test-utils').setup_environment;
 
 var client;
 
-var types = [
+var tests = [
     {
         type: 'int',
         value: 1,
@@ -29,6 +30,16 @@ var types = [
         type: 'timestamp',
         value: 1423515666128,
         code: types.CASS_VALUE_TYPE_TIMESTAMP
+    },
+    {
+        test: 'timestamp as object',
+        type: 'timestamp',
+        value: {
+            'low': 15666128,
+            'high': 14235
+        },
+        code: types.CASS_VALUE_TYPE_TIMESTAMP,
+        encoding: encodings.BIGINT_AS_OBJECT
     },
     {
         type: 'boolean',
@@ -57,42 +68,71 @@ var types = [
     },
     {
         type: 'bigint',
+        value: 2891546411504,
+        code: types.CASS_VALUE_TYPE_BIGINT
+    },
+    {
+        test: 'bigint as object',
+        type: 'bigint',
         value: {
             'low': 1546411504,
             'high': 286668
         },
-        code: types.CASS_VALUE_TYPE_BIGINT
+        code: types.CASS_VALUE_TYPE_BIGINT,
+        encoding: encodings.BIGINT_AS_OBJECT
     }
 ];
 
 // XXX should test all of the above inside a map.
 
-describe('hints', function() {
+describe('type conversions', function() {
     before(function() {
         client = new TestClient();
         return setup_environment(client);
     });
 
-    _.each(types, function(t) {
-        describe('supports ' + t.type, function() {
+    var i = 0;
+    _.each(tests, function(t) {
+        describe('supports ' + t.test || t.type, function() {
             var table = 'hint_test_' + t.type;
-            var fields = {key: 'varchar', data: t.type};
-            var hints = {data: t.code};
+            var fields = {key: 'varchar', value: t.type, other: 'int'};
+            var key_type = types.CASS_VALUE_TYPE_VARCHAR;
+            var value_type = t.code | (t.encoding || 0);
+            var other_type = types.CASS_VALUE_TYPE_INT;
 
             before(function() {
-                return client.createTable(table, fields, 'key');
+                return client.createTable(table, fields, 'key, other');
             });
 
-            _.each(['insertRows', 'insertRowsPrepared', 'insertRowsPreparedBatch'], function(method, index) {
-                var data = {key: 'test_key_' + method, data: t.value};
-                it('can add data with ' + method, function() {
-                    return client[method](table, [data], {hints: hints, batch_size: 1})
-                    .then(function() {
-                        return client.execute('select * from ' + table + ' where key=?;', [data.key])
-                    })
-                    .then(function(results) {
-                        var res = results.rows;
-                        expect(res[0]).deep.equal(data);
+            _.each(['insertRows', 'insertRowsPrepared', 'insertRowsPreparedBatch'], function(method) {
+                describe(method, function() {
+                    var data = {key: 'test_key_' + method, value: t.value, other: i++};
+                    it('can add data with ' + method, function() {
+                        return client[method](table, [data], {
+                            param_types: {key: key_type, value: value_type, other: other_type},
+                            batch_size: 1});
+                    });
+
+                    it('can query data by key', function() {
+                        var query = 'select key, value, other from ' + table + ' where key=?;';
+                        return client.execute(query, [data.key], {
+                                              param_types: [key_type],
+                                              result_types: [key_type, value_type, other_type]})
+                        .then(function(results) {
+                            var res = results.rows;
+                            expect(res[0]).deep.equal(data);
+                        });
+                    });
+
+                    it('can query data by key / other', function() {
+                        var query = 'select key, value, other from ' + table + ' where key=? AND other = ?;';
+                        return client.execute(query, [data.key, data.other], {
+                                              param_types: [key_type, other_type],
+                                              result_types: [key_type, value_type, other_type]})
+                        .then(function(results) {
+                            var res = results.rows;
+                            expect(res[0]).deep.equal(data);
+                        });
                     });
                 });
             });
